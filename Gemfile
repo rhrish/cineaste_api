a956d275e8dd89f834a5b8ea912459aaaf956d15
Harish R <dev@rhrish.com>
2022-11-20 12:40:03 +0100

    - Added CORS support - Translated fields in API - API bug fixes

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "https://rubygems.org"
 gem 'sinatra'
 gem "sinatra-activerecord"
 gem 'sinatra-contrib'
+gem 'sinatra-cors'
 gem 'rack'
 gem 'rake'
 gem 'mysql2'
--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 require "sinatra/namespace"
 require "sinatra/activerecord"
+require "sinatra/cors"
 require "cgi"
 
 # DB Setup
@@ -9,6 +10,12 @@ set :database_file, 'config/database.yml'
 # Environment
 set :bind, '127.0.0.1'
 set :port, 9494
+
+# CORS
+set :allow_origin, "http://localhost:5173"
+set :allow_methods, "GET,HEAD,POST"
+set :allow_headers, "content-type,if-modified-since"
+set :expose_headers, "location,link"
 
 # Model
 class Article < ActiveRecord::Base
@@ -38,25 +45,23 @@ end
 namespace '/api/v1' do
   before do
     content_type 'application/json'
+    headers 'Access-Control-Allow-Origin' => '*', 
+            'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST']
   end
 
   get '/articles' do
-    if params.length <= 0 then
-      articles = Article.all
-    else
-      articles = Article.all
-      [:category, :date, :location].each do |filter|
-        articles = articles.send(filter, params[filter]) if params[filter]
-      end
-   end
-    articles.to_json(:methods => :aid, :only => [ :id, :title, :category, :location, :published_date, :md_data])
+    articles = Article.all.order(published_date: :desc)
+    [:category, :date, :location].each do |filter|
+      articles = articles.send(filter, params[filter]) if params[filter]
+    end
+    articles.to_json(:methods => :aid, :only => [ :id, :title, :category, :location, :published_date, :md_data, :category_en, :title_en, :md_data_en])
   end
 
   get '/article/:id' do
     halt(404, { message:'Not Found'}.to_json) if(params.length<=0)
     article = Article.find_by(id: params[:id])
     halt(404, { message:'Not Found'}.to_json) if(article.nil?)
-    article.to_json(:methods => :aid, :only => [ :id, :title, :category, :location, :published_date, :md_data])
+    article.to_json(:methods => :aid, :only => [ :id, :title, :category, :location, :published_date, :md_data, :category_en, :title_en, :md_data_en])
   end
 
 
@@ -71,7 +76,7 @@ namespace '/api/v1' do
   end
 
   get '/dates' do
-    dates = Article.select(:published_date).distinct
+    dates = Article.select(:published_date).distinct.order(published_date: :desc)
     dates.to_json(:only => :published_date, :methods => :date_url)
   end
 
